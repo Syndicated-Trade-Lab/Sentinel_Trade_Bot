8c37d1d0365ab63b282e1a6d5d299ee6f045caee
Dev-Infinite-2048 <jpf99@protonmail.com>
2022-09-30 19:27:23 -0500

    Test api keystore complete as well as config base

--- Tests/Run.ps1
+++ Tests/Run.ps1
@@ -1,5 +1,7 @@
 # This script is used for setting up the system package requirements and installing missing libraries
-.\Setup_Config.ps1
+##.\Setup_Config.ps1
 
 # 
-python3 .\Setup\api_keystore_test.py
+python ..\Setup\api_keystore_test.py
+
+python ..\Setup\var_test.py
